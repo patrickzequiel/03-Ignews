5dda23067a8883b873c2f21764e208ac8055900f
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-10-13 11:40:45 +0200

    Pagamentos no stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,48 @@
+import { signIn, useSession } from 'next-auth/client'
+import { api } from '../../services/api';
+import { fauna } from '../../services/fauna';
+import { getStripeJs } from '../../services/stripe.js';
+import { query as q } from 'faunadb';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
+
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const [session] = useSession()
+
+    async function handleSubscribe() {
+
+        if (!session) {
+            signIn('github')
+            return;
+        }
+
+        //criação da checkout session
+        try {
+            const response = await api.post('/subscribe');
+      
+            const { sessionId } = response.data;
+      
+            const stripe = await getStripeJs();
+      
+            await stripe.redirectToCheckout({ sessionId });
+          } catch (err) {
+            console.log(err);
+            alert(err.message);
+          }
+        }
+
     return (
-        <button 
+        <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
-            Subscribe now 
+            Subscribe now
         </button>
     )
 }
